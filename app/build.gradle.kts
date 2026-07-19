e3ea3b6141f9bd88eca7f9abdfd2390adabae0ec
Mr-Destructive <gormeet711@gmail.com>
2026-07-19 17:59:10 +0530

    chore: bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.docdroid"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
